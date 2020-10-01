dc8402d6c721d276d144a3a4c2030045afdcff19
Xuanying Wang <71072255+SleeplessRegina@users.noreply.github.com>
2020-10-02 01:44:40 +0800

    Add files via upload

---
+++ Lab3/Simplify Path/leetcode 71/main12.java
@@ -0,0 +1,16 @@
+public class main12 {
+        public String simplifyPath(String path) {
+            Stack<String> s = new Stack<>();
+            String[] p = path.split("/");
+            for (String t : p) {
+                if (!s.isEmpty() && t.equals("..")) {
+                    s.pop();
+                } else if (!t.equals(".") && !t.equals("") && !t.equals("..")) {
+                    s.push(t);
+                }
+            }
+            List<String> list = new ArrayList(s);
+            return "/" + String.join("/", list);
+        }
+    }
+
