a5996efd34634976bf0de00f973af98265be6027
Xuanying Wang <71072255+SleeplessRegina@users.noreply.github.com>
2020-10-02 01:08:34 +0800

    Add files via upload

---
+++ Lab3/Reorder List/leetcode 143/main7.java
@@ -0,0 +1,27 @@
+public class main7 {
+        public void reorderList(ListNode *head) {
+            if (!head || !head->next || !head->next->next) return;
+            ListNode *fast = head, *slow = head;
+            while (fast->next && fast->next->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            ListNode *mid = slow->next;
+            slow->next = NULL;
+            ListNode *last = mid, *pre = NULL;
+            while (last) {
+                ListNode *next = last->next;
+                last->next = pre;
+                pre = last;
+                last = next;
+            }
+            while (head && pre) {
+                ListNode *next = head->next;
+                head->next = pre;
+                pre = pre->next;
+                head->next->next = next;
+                head = next;
+            }
+        }
+    };
+
