f5b9c4d9b85502ef342e31de3e7cd1a89221610e
Xuanying Wang <71072255+SleeplessRegina@users.noreply.github.com>
2020-10-02 00:32:26 +0800

    Add files via upload

---
+++ Lab3/Delete N Nodes After M Nodes of a Linked List/leetcode 1474/main1.java
@@ -0,0 +1,24 @@
+public class main1 {
+    public ListNode deleteNodes(ListNode head, int m, int n) {
+        ListNode pointer = new ListNode(0);
+        pointer.next = head;
+        while (pointer != null) {
+            pointer = getPost(pointer, m);
+            if (pointer == null) {
+                break;
+            }
+            ListNode postNode = getPost(pointer, n + 1);
+            pointer.next = postNode;
+        }
+        return head;
+    }
+
+    private ListNode getPost(ListNode node, int count) {
+        for (int i = 0; i < count && node != null; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+
+}
